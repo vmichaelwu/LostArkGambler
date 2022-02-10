ca878857f0edf50203b78804e7089e538e854b1a
MichaelWu <vmichael.wu@gmail.com>
2022-02-09 23:58:14 -0800

    fixing bugs

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './client/src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, './client/dist/'),
+    path: path.resolve(__dirname, 'client/dist/'),
   },
   module: {
     rules: [
